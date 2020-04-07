5e3030e4a94272bc9b5f62eee98ed1baf87d8e99
Sachin <mittalsam98@gmail.com>
2020-04-07 23:49:17 +0530

    added getData() method

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:climateapp/services/location.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -12,6 +12,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getPosition();
+    getData();
   }
 
   void getPosition() async {
@@ -21,17 +22,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.latitude);
   }
 
+  void getData() async {
+    http.Response response = await http.get(
+        'http://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=b6907d289e10d714a6e88b30761fae22');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getPosition();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
